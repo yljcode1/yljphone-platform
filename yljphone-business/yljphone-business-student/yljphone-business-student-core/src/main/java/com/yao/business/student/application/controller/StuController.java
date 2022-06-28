1023ecb7d69a31b685ae6e41856a4901129e06da
yljcode1 <aaa2826455837@gmail.com>
2022-06-28 22:45:01 +0800

    feat: 添加lombak日志打印

--- yljphone-business/yljphone-business-student/yljphone-business-student-core/src/main/java/com/yao/business/student/application/controller/StuController.java
+++ yljphone-business/yljphone-business-student/yljphone-business-student-core/src/main/java/com/yao/business/student/application/controller/StuController.java
@@ -1,5 +1,6 @@
 package com.yao.business.student.application.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stu")
 @RefreshScope
+@Slf4j
 public class StuController {
 
     @Value("${spring.datasource.url}")
@@ -26,6 +28,7 @@ public class StuController {
     public String list() {
         System.out.println("学生列表:");
         System.out.println(dbUrl);
+        log.info("测试打印日志信息");
         return "stu list";
     }
 }
