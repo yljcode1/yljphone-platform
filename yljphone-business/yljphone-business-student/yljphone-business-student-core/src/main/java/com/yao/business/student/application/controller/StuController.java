b0fddfa98eee525020eb474b10a59cd95c34fbea
yljcode1 <aaa2826455837@gmail.com>
2022-06-28 22:38:43 +0800

    feat: 添加nacos多环境

--- yljphone-business/yljphone-business-student/yljphone-business-student-core/src/main/java/com/yao/business/student/application/controller/StuController.java
+++ yljphone-business/yljphone-business-student/yljphone-business-student-core/src/main/java/com/yao/business/student/application/controller/StuController.java
@@ -1,6 +1,7 @@
 package com.yao.business.student.application.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/stu")
+@RefreshScope
 public class StuController {
 
     @Value("${spring.datasource.url}")
