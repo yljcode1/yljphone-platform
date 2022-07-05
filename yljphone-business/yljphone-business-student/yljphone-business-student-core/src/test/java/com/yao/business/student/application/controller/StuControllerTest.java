968ca05bf8530836e99e75619e81262f6f899977
yljcode1 <aaa2826455837@gmail.com>
2022-07-05 23:02:12 +0800

    feat: 添加mybatis基础test

--- yljphone-business/yljphone-business-student/yljphone-business-student-core/src/test/java/com/yao/business/student/application/controller/StuControllerTest.java
+++ yljphone-business/yljphone-business-student/yljphone-business-student-core/src/test/java/com/yao/business/student/application/controller/StuControllerTest.java
@@ -1,7 +1,13 @@
 package com.yao.business.student.application.controller;
 
 import com.yao.business.student.application.StuApplication;
+import com.yao.data.entity.Student;
+import com.yao.data.mapper.StudentDao;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +37,22 @@ class StuControllerTest {
     @Test
     void login() {
         stuController.login("zhangsan", "lisi");
+    }
+
+    /**
+     * mybatis原始
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testMybatis() throws IOException {
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory build = builder.build(Resources.getResourceAsReader("mybatisl-config.xml"));
+        SqlSession sqlSession = build.openSession();
+        StudentDao mapper = sqlSession.getMapper(StudentDao.class);
+        Student student = mapper.selectById(1);
+        log.info("{}", student);
+        sqlSession.commit();
+        sqlSession.close();
     }
 }
