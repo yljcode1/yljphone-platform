e60b073fe004c93d5d9d4c57707a756912243e1e
yljcode1 <aaa2826455837@gmail.com>
2022-08-15 20:11:34 +0800

    feat: 链式编程

--- yljphone-platform-explam/src/main/java/com/yao/juc/demo/CompletableFutureMallDemo.java
+++ yljphone-platform-explam/src/main/java/com/yao/juc/demo/CompletableFutureMallDemo.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  * 链式编程
  * Accessors
@@ -16,6 +18,13 @@ public class CompletableFutureMallDemo {
     public static void main(String[] args) {
         Student student = new Student();
         student.setStudentName("li4").setId(12).setMajor("English");
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            return "hello";
+        });
+        // get 编译时报错
+//        stringCompletableFuture.get();
+        // join编译时不报错
+        stringCompletableFuture.join();
     }
 }
 
