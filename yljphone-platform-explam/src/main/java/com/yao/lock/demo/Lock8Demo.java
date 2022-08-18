1a56e60b41120c4c34a8eac0811de3a16c1abf0b
yljcode1 <aaa2826455837@gmail.com>
2022-08-18 22:50:10 +0800

    feat: 锁的8钟案例总结

--- yljphone-platform-explam/src/main/java/com/yao/lock/demo/Lock8Demo.java
+++ yljphone-platform-explam/src/main/java/com/yao/lock/demo/Lock8Demo.java
@@ -13,12 +13,19 @@ import java.util.concurrent.TimeUnit;
  * 8锁案例说明：
  * 1、标准访问有ab两个线程，请问先打印邮箱还是短信
  * 2、sendEmail方法中加入暂停3秒钟，请问先打印邮件还是短信
+ * 有且只有一个线程能够使用资源类（加上synchronized方法的都会等待）
  * 3、添加一个普通方法hello，请问先打印邮箱还是hello
  * 4、有两部手机，先打印邮件还是短信
  * 5、两个静态同步方法，1部手机，请问先打印邮件还是短信
  * 6、两个静态同步方法，2部手机，请问先打印邮件还是短信
  * 7、有1个静态同步方法，有1个普通同步方法，有1部手机，请问先打印邮箱还是短信
  * 8、有1个静态同步方法，有一个普通同步方法，有两部手机，先打印邮箱还是短信
+ * <p>
+ * 笔记总结：
+ * 1-2
+ * 一个对象里面如果有多个synchronized方法，某一时刻内，只要一个线程去调用其中的synchronized方法，其他线程只能等待，
+ * 换一句话说，某一个时刻内，只能有唯一一个线程去访问折现synchronized方法。锁的是当前对象this，被锁定后，其他的线程都不能
+ * 进入当前对象的其他的synchronized方法
  */
 public class Lock8Demo {
     public static void main(String[] args) {
@@ -34,7 +41,8 @@ public class Lock8Demo {
 }
 
 
-class Phone {
+class Phone // 资源类
+{
     public synchronized void sendEmail() {
 //        try {
 //            TimeUnit.SECONDS.sleep(3);
@@ -46,5 +54,9 @@ class Phone {
 
     public synchronized void sendSMS() {
         System.out.println("----------sendSMS");
+    }
+
+    public void hello() {
+        System.out.println("----hello----");
     }
 }
